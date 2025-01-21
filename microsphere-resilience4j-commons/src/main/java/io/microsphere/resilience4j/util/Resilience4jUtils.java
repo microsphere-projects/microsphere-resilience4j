ea40541193b38c741dfbdb6c25256f94dd2c3a74
Mercy Ma <mercyblitz@gmail.com>
2025-01-21 19:09:57 +0800

    Update Resilience4jUtils.java

--- microsphere-resilience4j-commons/src/main/java/io/microsphere/resilience4j/util/Resilience4jUtils.java
+++ microsphere-resilience4j-commons/src/main/java/io/microsphere/resilience4j/util/Resilience4jUtils.java
@@ -17,16 +17,13 @@
 package io.microsphere.resilience4j.util;
 
 import io.github.resilience4j.core.Registry;
-import io.microsphere.reflect.MethodUtils;
 import io.microsphere.resilience4j.common.Resilience4jModule;
 import io.microsphere.util.BaseUtils;
 
-import java.lang.invoke.MethodHandle;
 import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 
-import static io.microsphere.invoke.MethodHandleUtils.findVirtual;
 import static io.microsphere.reflect.MethodUtils.findMethod;
 import static io.microsphere.reflect.MethodUtils.invokeMethod;
 import static io.microsphere.resilience4j.common.Resilience4jModule.valueOf;
@@ -42,25 +39,28 @@ import static java.util.Collections.unmodifiableMap;
  */
 public abstract class Resilience4jUtils extends BaseUtils {
 
-
     private static final Map<Resilience4jModule, Method> getEntryMethodsCache;
 
     static {
         Resilience4jModule[] modules = Resilience4jModule.values();
         Map<Resilience4jModule, Method> methodsCache = new HashMap<>(modules.length);
         for (Resilience4jModule module : modules) {
-            initGetEntryMethodHandlesCache(module, methodsCache);
+            initGetEntryMethodsCache(module, methodsCache);
         }
         getEntryMethodsCache = unmodifiableMap(methodsCache);
     }
 
-    private static void initGetEntryMethodHandlesCache(Resilience4jModule module, Map<Resilience4jModule, Method> methodsCache) {
+    private static void initGetEntryMethodsCache(Resilience4jModule module, Map<Resilience4jModule, Method> methodsCache) {
         Class<?> entryClass = module.getEntryClass();
         Class<?> configClass = module.getConfigurationClass();
         Class<?> registryClass = module.getRegistryClass();
         String methodName = decapitalize(entryClass.getSimpleName());
         Method method = findMethod(registryClass, methodName, String.class, configClass);
         methodsCache.put(module, method);
+    }
+
+    public static <E, C> E getEntry(Registry<E, C> registry, String name) {
+        return getEntry(registry, name, registry.getDefaultConfig());
     }
 
     public static <E, C> E getEntry(Registry<E, C> registry, String name, C configuration) {

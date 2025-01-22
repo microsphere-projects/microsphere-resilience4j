51770391116798c2da616da65423a691f6602339
Mercy Ma <mercyblitz@gmail.com>
2025-01-22 22:48:32 +0800

    Create RateLimiterDruidFilter.java

---
+++ microsphere-resilience4j-spring/src/main/java/io/microsphere/resilience4j/spring/ratelimiter/jdbc/druid/RateLimiterDruidFilter.java
@@ -0,0 +1,66 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.microsphere.resilience4j.spring.ratelimiter.jdbc.druid;
+
+import com.alibaba.druid.filter.Filter;
+import io.github.resilience4j.ratelimiter.RateLimiter;
+import io.github.resilience4j.ratelimiter.RateLimiterConfig;
+import io.github.resilience4j.ratelimiter.RateLimiterRegistry;
+import io.microsphere.resilience4j.spring.common.jdbc.druid.Resilience4jDruidFilter;
+
+import static io.github.resilience4j.ratelimiter.RateLimiter.waitForPermission;
+
+/**
+ * {@link RateLimiter} x Druid {@link Filter}
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
+ * @since 1.0.0
+ */
+public class RateLimiterDruidFilter extends Resilience4jDruidFilter<RateLimiter, RateLimiterConfig, RateLimiterRegistry> {
+
+    public RateLimiterDruidFilter(RateLimiterRegistry registry) {
+        super(registry);
+    }
+
+    @Override
+    protected RateLimiter createEntry(String name) {
+        RateLimiterRegistry registry = super.getRegistry();
+        return registry.rateLimiter(name, super.getConfiguration(name), registry.getTags());
+    }
+
+    @Override
+    protected void beforeExecute(RateLimiter rateLimiter) {
+        waitForPermission(rateLimiter);
+    }
+
+    @Override
+    protected void afterExecute(RateLimiter rateLimiter, long duration, Object result, Throwable failure) {
+        if (failure == null) {
+            if (result != null) {
+                // Success with result
+                rateLimiter.onResult(result);
+            } else {
+                // Success without result
+                rateLimiter.onSuccess();
+            }
+        } else {
+            // On error
+            rateLimiter.onError(failure);
+        }
+    }
+}
+

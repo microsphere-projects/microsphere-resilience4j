2e3dd02be47c08f7079874843a9f8cdb178864c8
Mercy Ma <mercyblitz@gmail.com>
2025-01-26 12:13:01 +0800

    Update Resilience4jUtilsTest.java

--- microsphere-resilience4j-commons/src/test/java/io/microsphere/resilience4j/util/Resilience4jUtilsTest.java
+++ microsphere-resilience4j-commons/src/test/java/io/microsphere/resilience4j/util/Resilience4jUtilsTest.java
@@ -19,11 +19,40 @@ package io.microsphere.resilience4j.util;
 
 import io.github.resilience4j.bulkhead.Bulkhead;
 import io.github.resilience4j.bulkhead.BulkheadRegistry;
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
+import io.github.resilience4j.core.EventPublisher;
+import io.github.resilience4j.ratelimiter.RateLimiter;
+import io.github.resilience4j.ratelimiter.RateLimiterRegistry;
+import io.github.resilience4j.retry.Retry;
+import io.github.resilience4j.retry.RetryRegistry;
+import io.github.resilience4j.timelimiter.TimeLimiter;
+import io.github.resilience4j.timelimiter.TimeLimiterRegistry;
+import io.microsphere.resilience4j.bulkhead.BulkheadTemplate;
+import io.microsphere.resilience4j.circuitbreaker.CircuitBreakerTemplate;
+import io.microsphere.resilience4j.common.Resilience4jModule;
+import io.microsphere.resilience4j.common.Resilience4jTemplate;
+import io.microsphere.resilience4j.ratelimiter.RateLimiterTemplate;
+import io.microsphere.resilience4j.retry.RetryTemplate;
+import io.microsphere.resilience4j.timelimiter.TimeLimiterTemplate;
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
+
 import static io.github.resilience4j.bulkhead.BulkheadRegistry.ofDefaults;
+import static io.microsphere.resilience4j.common.Resilience4jModule.BULKHEAD;
+import static io.microsphere.resilience4j.common.Resilience4jModule.CIRCUIT_BREAKER;
+import static io.microsphere.resilience4j.common.Resilience4jModule.RATE_LIMITER;
+import static io.microsphere.resilience4j.common.Resilience4jModule.RETRY;
+import static io.microsphere.resilience4j.common.Resilience4jModule.TIME_LIMITER;
 import static io.microsphere.resilience4j.util.Resilience4jUtils.getEntry;
+import static io.microsphere.resilience4j.util.Resilience4jUtils.getEventProcessor;
+import static io.microsphere.resilience4j.util.Resilience4jUtils.loadDefaultTemplates;
+import static java.util.EnumSet.allOf;
+import static java.util.EnumSet.copyOf;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
  * {@link Resilience4jUtils} Test
@@ -35,6 +64,17 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 public class Resilience4jUtilsTest {
 
     @Test
+    public void testLoadDefaultTemplates() {
+        Map<Resilience4jModule, Class<? extends Resilience4jTemplate>> defaultTemplates = loadDefaultTemplates();
+        assertEquals(allOf(Resilience4jModule.class), copyOf(defaultTemplates.keySet()));
+        assertEquals(defaultTemplates.get(RETRY), RetryTemplate.class);
+        assertEquals(defaultTemplates.get(CIRCUIT_BREAKER), CircuitBreakerTemplate.class);
+        assertEquals(defaultTemplates.get(RATE_LIMITER), RateLimiterTemplate.class);
+        assertEquals(defaultTemplates.get(TIME_LIMITER), TimeLimiterTemplate.class);
+        assertEquals(defaultTemplates.get(BULKHEAD), BulkheadTemplate.class);
+    }
+
+    @Test
     public void testGetEntry() {
         BulkheadRegistry bulkheadRegistry = ofDefaults();
         Bulkhead bulkhead = getEntry(bulkheadRegistry, "test");
@@ -43,6 +83,51 @@ public class Resilience4jUtilsTest {
 
     @Test
     public void testGetEventPublisher() {
+        RetryRegistry retryRegistry = RetryRegistry.ofDefaults();
+        assertEquals(retryRegistry.getEventPublisher(), getEventProcessor(retryRegistry));
 
+        TimeLimiterRegistry timeLimiterRegistry = TimeLimiterRegistry.ofDefaults();
+        assertEquals(timeLimiterRegistry.getEventPublisher(), getEventProcessor(timeLimiterRegistry));
+
+        RateLimiterRegistry rateLimiterRegistry = RateLimiterRegistry.ofDefaults();
+        assertEquals(rateLimiterRegistry.getEventPublisher(), getEventProcessor(rateLimiterRegistry));
+
+        BulkheadRegistry bulkheadRegistry = BulkheadRegistry.ofDefaults();
+        assertEquals(bulkheadRegistry.getEventPublisher(), getEventProcessor(bulkheadRegistry));
+
+        CircuitBreakerRegistry circuitBreakerRegistry = CircuitBreakerRegistry.ofDefaults();
+        assertEquals(circuitBreakerRegistry.getEventPublisher(), getEventProcessor(circuitBreakerRegistry));
+
+        assertThrows(UnsupportedOperationException.class, () -> getEventProcessor(new Object()));
+        assertThrows(UnsupportedOperationException.class, () -> getEventProcessor((EventPublisher) onEventConsumer -> {
+        }));
+
+        String name = "test";
+
+
+        Retry retry = retryRegistry.retry(name);
+        Object entry = retry;
+        assertEquals(retry.getEventPublisher(), getEventProcessor(retry));
+        assertEquals(retry.getEventPublisher(), getEventProcessor(entry));
+
+        TimeLimiter timeLimiter = timeLimiterRegistry.timeLimiter(name);
+        entry = timeLimiter;
+        assertEquals(timeLimiter.getEventPublisher(), getEventProcessor(timeLimiter));
+        assertEquals(timeLimiter.getEventPublisher(), getEventProcessor(entry));
+
+        RateLimiter rateLimiter = rateLimiterRegistry.rateLimiter(name);
+        entry = rateLimiter;
+        assertEquals(rateLimiter.getEventPublisher(), getEventProcessor(rateLimiter));
+        assertEquals(rateLimiter.getEventPublisher(), getEventProcessor(entry));
+
+        Bulkhead bulkhead = bulkheadRegistry.bulkhead(name);
+        entry = bulkhead;
+        assertEquals(bulkhead.getEventPublisher(), getEventProcessor(bulkhead));
+        assertEquals(bulkhead.getEventPublisher(), getEventProcessor(entry));
+
+        CircuitBreaker circuitBreaker = circuitBreakerRegistry.circuitBreaker(name);
+        entry = circuitBreaker;
+        assertEquals(circuitBreaker.getEventPublisher(), getEventProcessor(circuitBreaker));
+        assertEquals(circuitBreaker.getEventPublisher(), getEventProcessor(entry));
     }
 }

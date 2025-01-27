298999efc3387ae949b8e6384b320de7aac58cbb
Mercy Ma <mercyblitz@gmail.com>
2025-01-27 21:40:34 +0800

    Update RateLimiterTemplate.java

--- microsphere-resilience4j-commons/src/main/java/io/microsphere/resilience4j/ratelimiter/RateLimiterTemplate.java
+++ microsphere-resilience4j-commons/src/main/java/io/microsphere/resilience4j/ratelimiter/RateLimiterTemplate.java
@@ -16,9 +16,13 @@
  */
 package io.microsphere.resilience4j.ratelimiter;
 
+import io.github.resilience4j.core.EventConsumer;
 import io.github.resilience4j.ratelimiter.RateLimiter;
 import io.github.resilience4j.ratelimiter.RateLimiterConfig;
 import io.github.resilience4j.ratelimiter.RateLimiterRegistry;
+import io.github.resilience4j.ratelimiter.event.RateLimiterOnDrainedEvent;
+import io.github.resilience4j.ratelimiter.event.RateLimiterOnFailureEvent;
+import io.github.resilience4j.ratelimiter.event.RateLimiterOnSuccessEvent;
 import io.microsphere.resilience4j.common.Resilience4jContext;
 import io.microsphere.resilience4j.common.Resilience4jTemplate;
 
@@ -76,5 +80,41 @@ public class RateLimiterTemplate extends Resilience4jTemplate<RateLimiter, RateL
         } else {
             rateLimiter.onError(failure);
         }
+    }
+
+    /**
+     * Register the {@link EventConsumer} of {@link RateLimiterOnSuccessEvent}
+     *
+     * @param entryName     the name of entry
+     * @param eventConsumer the {@link EventConsumer} of {@link RateLimiterOnSuccessEvent}
+     * @return {@link RateLimiterTemplate}
+     */
+    public RateLimiterTemplate onSuccessEvent(String entryName, EventConsumer<RateLimiterOnSuccessEvent> eventConsumer) {
+        registerEntryEventConsumer(entryName, RateLimiterOnSuccessEvent.class, eventConsumer);
+        return this;
+    }
+
+    /**
+     * Register the {@link EventConsumer} of {@link RateLimiterOnFailureEvent}
+     *
+     * @param entryName     the name of entry
+     * @param eventConsumer the {@link EventConsumer} of {@link RateLimiterOnFailureEvent}
+     * @return {@link RateLimiterTemplate}
+     */
+    public RateLimiterTemplate onFailureEvent(String entryName, EventConsumer<RateLimiterOnFailureEvent> eventConsumer) {
+        registerEntryEventConsumer(entryName, RateLimiterOnFailureEvent.class, eventConsumer);
+        return this;
+    }
+
+    /**
+     * Register the {@link EventConsumer} of {@link RateLimiterOnDrainedEvent}
+     *
+     * @param entryName     the name of entry
+     * @param eventConsumer the {@link EventConsumer} of {@link RateLimiterOnFailureEvent}
+     * @return {@link RateLimiterTemplate}
+     */
+    public RateLimiterTemplate onDrainedEvent(String entryName, EventConsumer<RateLimiterOnDrainedEvent> eventConsumer) {
+        registerEntryEventConsumer(entryName, RateLimiterOnDrainedEvent.class, eventConsumer);
+        return this;
     }
 }

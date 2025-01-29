f721bf4ad08906a4bde39a24dc9641eb64055f99
Mercy Ma <mercyblitz@gmail.com>
2025-01-30 01:03:48 +0800

    Update TimeLimiterTemplateTest.java

--- microsphere-resilience4j-commons/src/test/java/io/microsphere/resilience4j/timelimiter/TimeLimiterTemplateTest.java
+++ microsphere-resilience4j-commons/src/test/java/io/microsphere/resilience4j/timelimiter/TimeLimiterTemplateTest.java
@@ -20,6 +20,17 @@ import io.github.resilience4j.timelimiter.TimeLimiter;
 import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 import io.github.resilience4j.timelimiter.TimeLimiterRegistry;
 import io.microsphere.resilience4j.common.AbstractResilience4jTemplateTest;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static io.github.resilience4j.timelimiter.event.TimeLimiterEvent.Type.ERROR;
+import static io.github.resilience4j.timelimiter.event.TimeLimiterEvent.Type.SUCCESS;
+import static io.github.resilience4j.timelimiter.event.TimeLimiterEvent.Type.TIMEOUT;
+import static java.time.Duration.ofMillis;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * {@link TimeLimiterTemplate} Test
@@ -29,4 +40,69 @@ import io.microsphere.resilience4j.common.AbstractResilience4jTemplateTest;
  * @since 1.0.0
  */
 public class TimeLimiterTemplateTest extends AbstractResilience4jTemplateTest<TimeLimiter, TimeLimiterConfig, TimeLimiterRegistry, TimeLimiterTemplate> {
+
+    private final long timeoutDurationInMills = 50;
+
+    private final Duration timeoutDuration = ofMillis(timeoutDurationInMills);
+
+    /**
+     * Create an instance of {@link TimeLimiterConfig} for testing
+     *
+     * @return non-null
+     */
+    @Override
+    protected TimeLimiterConfig createEntryConfig() {
+        return TimeLimiterConfig.custom()
+                .cancelRunningFuture(true)
+                .timeoutDuration(timeoutDuration)
+                .build();
+    }
+
+    @Test
+    public void testExecute() {
+        String entryName = this.entryName;
+        TimeLimiterTemplate template = this.template;
+
+        template.onSuccessEvent(entryName, event -> {
+            logEvent(entryName);
+            assertEquals(entryName, event.getTimeLimiterName());
+            assertSame(SUCCESS, event.getEventType());
+        });
+
+        template.execute(entryName, () -> {
+        });
+    }
+
+    @Test
+    public void testExecuteOnFailed() {
+        String entryName = this.entryName;
+        TimeLimiterTemplate template = this.template;
+
+        template.onErrorEvent(entryName, event -> {
+            logEvent(entryName);
+            assertEquals(entryName, event.getTimeLimiterName());
+            assertSame(ERROR, event.getEventType());
+            assertTrue(event.getThrowable() instanceof RuntimeException);
+        });
+
+        template.execute(entryName, () -> {
+            throw new RuntimeException("For Testing");
+        });
+    }
+
+    @Test
+    public void testExecuteOnTimeout() {
+        String entryName = this.entryName;
+        TimeLimiterTemplate template = this.template;
+
+        template.onTimeoutEvent(entryName, event -> {
+            logEvent(entryName);
+            assertEquals(entryName, event.getTimeLimiterName());
+            assertSame(TIMEOUT, event.getEventType());
+        });
+
+        template.execute(entryName, () -> {
+            await(timeoutDurationInMills * 2);
+        });
+    }
 }

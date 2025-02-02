f52b99070946c358e7f2aef4c67ac74356f874c5
Mercy Ma <mercyblitz@gmail.com>
2025-02-03 02:20:44 +0800

    Add module 'microsphere-resilience4j-feign'

--- microsphere-resilience4j-feign/src/main/java/io/microsphere/resilience4j/feign/Resilience4jCapability.java
+++ microsphere-resilience4j-feign/src/main/java/io/microsphere/resilience4j/feign/Resilience4jCapability.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.microsphere.resilience4j.integration.feign;
+package io.microsphere.resilience4j.feign;
 
 import feign.Capability;
 import feign.InvocationHandlerFactory;
